142693e60afcfdf15d106c8cdcda8bf013a36f78
Mati Viscusso <mati@novolabs.xyz>
2021-09-14 14:18:34 -0300

    change offices

--- src/components/OurServices/index.jsx
+++ src/components/OurServices/index.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
-import SectionTittle from "../SectionTittle";
-import iconOffice from "../../assets/svg/office-building.svg";
-import iconSpaces from "../../assets/svg/workspace.svg";
-import iconWorkspaces from "../../assets/svg/reunion.svg";
-import iconFotografia from "../../assets/svg/camara.svg";
-import imgSpaces from "../../assets/img/nomade.jpg";
-import imgResidente from "../../assets/img/residente.jpg";
+import imgFotografia from "../../assets/img/estudio-fotografico.jpg";
 import imgfixdesk from "../../assets/img/fixdesk.jpg";
+import imgSpaces from "../../assets/img/nomade.jpg";
+import imgOffices from "../../assets/img/ofi.jpg";
+import imgResidente from "../../assets/img/residente.jpg";
 import imgWorkspaces from "../../assets/img/reu.jpg";
 import imgReu from "../../assets/img/sala-reu.jpg";
-import imgOffices from "../../assets/img/ofi.jpg";
-import imgFotografia from "../../assets/img/estudio-fotografico.jpg";
+import iconFotografia from "../../assets/svg/camara.svg";
+import iconOffice from "../../assets/svg/office-building.svg";
+import iconWorkspaces from "../../assets/svg/reunion.svg";
+import iconSpaces from "../../assets/svg/workspace.svg";
+import SectionTittle from "../SectionTittle";
 
 export default function OurServices() {
     const [isVisible, setIsVisible] = useState(1);
@@ -263,7 +263,6 @@ export default function OurServices() {
                             </ul>
                             <br />
 
-                            <p>OFICINA GRANDE (Hasta 8 personas)</p>
                             <h3>$3.000 + I.V.A. - Jornada completa</h3>
                             <h3>$1.500 + I.V.A. - Media Jornada</h3>
                         </div>
